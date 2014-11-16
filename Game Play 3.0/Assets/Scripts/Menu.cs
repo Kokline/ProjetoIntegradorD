81903810c34495cc079094ed913c976e229e8c61
GusCunha <gustavocunha87@gmail.com>
2014-11-16 21:26:06 -0200

    Menu
    Menu

---
+++ Game Play 3.0/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+   public GameCamera cam;
+
+   void OnMouseEnter()
+   {
+      cam.SetZoom(true, transform);
+   }
+
+   void OnMouseExit()
+   {
+      cam.SetZoom(false, null);
+   }
+}
