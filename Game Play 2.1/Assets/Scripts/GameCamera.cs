61fb89aa1c015d9ade0e5ac18f53325a2ba37a0a
Michael Lennon de Moura <kokline3000@hotmail.com>
2014-11-16 19:31:45 -0200

    Versao 2.1
    Nessa versão a movimentação do personagem foi completamente corrigida.

---
+++ Game Play 2.1/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Comportamento da camera
+/// </summary>
+public class GameCamera : MonoBehaviour
+{
+   private Transform target;
+   public float trackSpeed = 25;
+
+   /// <summary>
+   /// Define o alvo da camera
+   /// </summary>
+   /// <param name="t"></param>
+   public void SetTarget(Transform t)
+   {
+      target = t;
+      transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
+   }
+
+   /// <summary>
+   /// Segue os movimentos do personagem
+   /// </summary>
+   void LateUpdate()
+   {
+      if (target)
+      {
+         float x = IncrementTowards(transform.position.x, target.position.x, trackSpeed);
+         float y = IncrementTowards(transform.position.y, target.position.y, trackSpeed);
+         transform.position = new Vector3(x, y, transform.position.z);
+      }
+   }
+
+   /// <summary>
+   /// Incrementa a posição da camera para seguir o personagem
+   /// </summary>
+   /// <param name="currentPosition"></param>
+   /// <param name="target"></param>
+   /// <param name="acceleration"></param>
+   /// <returns></returns>
+   private float IncrementTowards(float currentPosition, float target, float acceleration)
+   {
+      if (currentPosition == target)
+      {
+         return currentPosition;
+      }
+      else
+      {
+         float direction = Mathf.Sign(target - currentPosition); // must n be increased or decreased to get closer to target
+         currentPosition += acceleration * Time.deltaTime * direction;
+         return (direction == Mathf.Sign(target - currentPosition)) ? currentPosition : target; // if n has now passed target then return target, otherwise return n
+      }
+   }
+}
---
+++ Game Play 2.1/Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Ragdoll
+/// </summary>
+public class Ragdoll : MonoBehaviour
+{
+   private List<Transform> poseBones = new List<Transform>();
+   private List<Transform> ragdollBones = new List<Transform>();
+
+   /// <summary>
+   /// Copia a pose atual do player para o ragdoll
+   /// </summary>
+   /// <param name="pose"></param>
+   public void CopyPose(Transform pose)
+   {
+      AddChildren(pose, poseBones);
+      AddChildren(transform, ragdollBones);
+
+      foreach (Transform b in poseBones)
+      {
+         foreach (Transform r in ragdollBones)
+         {
+            if (r.name == b.name)
+            {
+               r.eulerAngles = b.eulerAngles;
+               break;
+            }
+         }
+      }
+   }
+
+   /// <summary>
+   /// Adiciona os bones do ragdoll na mesma posição dos bones do player
+   /// </summary>
+   /// <param name="parent"></param>
+   /// <param name="list"></param>
+   private void AddChildren(Transform parent, List<Transform> list)
+   {
+      list.Add(parent);
+      foreach (Transform t in parent)
+      {
+         AddChildren(t, list);
+      }
+   }
+}
